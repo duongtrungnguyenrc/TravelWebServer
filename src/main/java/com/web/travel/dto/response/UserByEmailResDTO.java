b67037ee58571911a800f986cec0728d2ad4b018
VietTran8 <cubon882003@gmail.com>
2023-10-12 22:29:28 +0700

    Added get user by email api

---
+++ src/main/java/com/web/travel/dto/response/UserByEmailResDTO.java
@@ -0,0 +1,13 @@
+package com.web.travel.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserByEmailResDTO {
+    private String name;
+    private String mail;
+}
