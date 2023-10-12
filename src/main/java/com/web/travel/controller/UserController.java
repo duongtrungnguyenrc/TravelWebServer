1fbcfc438fe731bd1f0b4eb085cdd591b1c50564
VietTran8 <cubon882003@gmail.com>
2023-10-12 22:33:20 +0700

    Added get user by email api

--- src/main/java/com/web/travel/controller/UserController.java
+++ src/main/java/com/web/travel/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     UserService service;
